abae90a1049fe24f7cf7d1e590e279b48ca5f54b
royharel2233 <royharel22@gmail.com>
2020-02-04 12:32:43 +0200

    css fixes

--- src/views/BuildShadePage/View.tsx
+++ src/views/BuildShadePage/View.tsx
@@ -6,24 +6,25 @@ import Page from "./Page";
 // import { TypedCollectionProductsQuery } from "./queries";
 
 
+import {
+  Online,
+} from "../../components";
 
 import {ChooseFabricQuery} from "./queries"
 
-
-export const View: React.FC = () => (
-  <>
+export const View: React.FC = ({}) => (
+  <Online>
     <div className="home-page">
-    <ChooseFabricQuery>
-      {({data, loading}) => {
-          return (
-              <Page
-                categories={data.categories}
-              />
-          );
-      }}
-    </ChooseFabricQuery>
-  </div>
-  <br />
-  </>
+      <ChooseFabricQuery>
+        {({data, loading}) => {
+            return (
+                <Page
+                  categories={data.categories}
+                />
+            );
+        }}
+      </ChooseFabricQuery>
+    </div>
+  </Online>
 );
 export default View;
